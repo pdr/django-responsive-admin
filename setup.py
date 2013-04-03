8e8be6c59368dad79443b5b82889cc947db6deee
Pierre <pierre.drescher@gmail.com>
2013-04-03 11:07:39 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-responsive-admin',
+    version='0.1',
+    description='A responsive admin for Django using Kube CSS',
+    long_description=open('README.rst').read(),
+    author='Pierre Drescher',
+    author_email='pierre.drescher@gmail.com',
+    url='https://github.com/pdr/django-responsive-admin',
+    license='MIT',
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=[],
+    include_package_data=True,
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities',
+    ]
+)
