6bc2a9637a42fe2d89d1201973d854466f8ca7a0
Pierre <pierre.drescher@gmail.com>
2013-04-04 15:46:52 -0400

    minor css updates

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-responsive-admin',
-    version='0.1.4',
+    version='0.1.5',
     description='A responsive admin for Django using Kube CSS',
     author='Pierre Drescher',
     author_email='pierre.drescher@gmail.com',
@@ -15,7 +15,7 @@ setup(
     classifiers=[
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
